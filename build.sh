92852619cbd50534ccf362d6cff25c325ad014fe
Mambix Ltd <ledi.mambix@gmail.com>
2018-12-29 13:46:22 +0000

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,3 +2,7 @@ git clone --recursive https://github.com/graft-project/graft-ng.git
 cd graft-ng
 git checkout alpha3
 git submodule update --init --recursive
+mkdir build
+cd build
+cmake -DENABLE_SYSLOG=ON ..
+make -j4
